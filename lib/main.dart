4f782beeb8079a09e4eb72bc732581cb48023554
rafabrunof <rafabrunofelix@hotmail.com>
2019-05-10 08:30:58 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:busca_gif/ui/homepage.dart';
+import 'package:busca_gif/ui/gif_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
